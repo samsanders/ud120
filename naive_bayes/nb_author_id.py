347e2e19576c82c8673afac6902278fe08d99672
samsanders <samsanders@charter.net>
2015-07-26 13:12:55 -0700

    Update nb_author_id.py

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -29,8 +29,6 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 #test
 
-test test
-
 #########################################################
 
 
