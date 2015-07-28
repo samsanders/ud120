25131e8edf4fd658ff071bda9dacb15d09a0b351
samsanders <samsanders@charter.net>
2015-07-27 20:25:40 -0700

    lesson 2 done

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,6 +26,33 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+clf = SVC(C = 10000, kernel = 'rbf')
+
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0,3), "s"
+
+t1 = time()
+accuracy = clf.score(features_test, labels_test)
+print "predicting time:", round(time()-t1,3), "s"
+
+print "accuracy:", accuracy
+
+
+predictions = clf.predict(features_test)
+
+print "prediction 10:", predictions[10], '\n', "prediction 26:", predictions[26], '\n', "prediction 50:", predictions[50]
+
+chris_predictions = predictions[predictions[:] == 1]
+
+print "number of chris predictions:", len(chris_predictions)
+
+
+
 
 #########################################################
 
