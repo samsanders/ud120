a2a859382e821e5ec6e7bff3adeb9bcbb127a01b
samsanders <samsanders@charter.net>
2015-07-27 20:26:23 -0700

    lesson 2 done

--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -38,7 +38,7 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
     words_file_handler.close()
 
     ### test_size is the percentage of events assigned to the test set (remainder go into training)
-    features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.15, random_state=42)
+    features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.10, random_state=42)
 
 
 
