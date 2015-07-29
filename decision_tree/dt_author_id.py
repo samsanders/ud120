942e3f1889e474b101425effc376e16023f8a1a8
samsanders <samsanders@charter.net>
2015-07-28 20:34:20 -0700

    lesson 3 done

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -26,6 +26,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+
+print "number of features:", len(features_train[0])
+
+clf = tree.DecisionTreeClassifier(min_samples_split = 40)
+clf.fit(features_train, labels_train)
+
+acc = clf.score(features_test, labels_test)
+print "accuracy:", acc
+
+
 
 
 #########################################################
